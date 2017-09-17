45c29f0473b4b7206c20bdc7a5be7a75e9045298
Austin Sarmento <austins0692@gmail.com>
2017-09-17 12:57:40 -0400

    scalling isn't terrible

--- app/src/main/java/com/jzheadley/augmentedshopper/googlevision/MultiTrackerActivity.java
+++ app/src/main/java/com/jzheadley/augmentedshopper/googlevision/MultiTrackerActivity.java
@@ -315,8 +315,9 @@ public final class MultiTrackerActivity extends AppCompatActivity implements Bar
             public void run() {
                 final LinearLayout layout = (LinearLayout) findViewById(R.id.btn_group);
                 layout.setVisibility(View.VISIBLE);
-                layout.setX(100);
+                layout.setX(50);
                 layout.setY(100);
+                layout.setScaleX(.7f);
                 Log.d(TAG, "run: " + layout.getX() + "\t" + layout.getY());
 
                 layout.bringToFront();
