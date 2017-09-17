0c34c7c5e8bff502e852207fd62506f79eba74e0
J. Zephyr Headley <headleyjz@vcu.edu>
2017-09-17 11:49:50 -0400

    upc api done

--- app/src/main/java/com/jzheadley/augmentedshopper/services/api/Offer.java
+++ app/src/main/java/com/jzheadley/augmentedshopper/services/api/Offer.java
@@ -39,4 +39,111 @@ public class Offer {
     @Expose
     public Integer updatedT;
 
+    public Offer() {
+    }
+
+    public String getMerchant() {
+        return merchant;
+    }
+
+    public void setMerchant(String merchant) {
+        this.merchant = merchant;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getListPrice() {
+        return listPrice;
+    }
+
+    public void setListPrice(String listPrice) {
+        this.listPrice = listPrice;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getShipping() {
+        return shipping;
+    }
+
+    public void setShipping(String shipping) {
+        this.shipping = shipping;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Integer getUpdatedT() {
+        return updatedT;
+    }
+
+    public void setUpdatedT(Integer updatedT) {
+        this.updatedT = updatedT;
+    }
+
+    @Override
+    public String toString() {
+        return "Offer{" +
+                "merchant='" + merchant + '\'' +
+                ", domain='" + domain + '\'' +
+                ", title='" + title + '\'' +
+                ", currency='" + currency + '\'' +
+                ", listPrice='" + listPrice + '\'' +
+                ", price=" + price +
+                ", shipping='" + shipping + '\'' +
+                ", condition='" + condition + '\'' +
+                ", availability='" + availability + '\'' +
+                ", link='" + link + '\'' +
+                ", updatedT=" + updatedT +
+                '}';
+    }
 }
